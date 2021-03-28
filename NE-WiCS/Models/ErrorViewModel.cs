9b0f2a21618536cb9736f1ba7a13b3bba1b62029
Kirsten Allanson <Kirsten_Allanson@hotmail.co.uk>
2021-03-28 11:40:08 +0100

    Trying to make it MVC

---
+++ NE-WiCS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NE_WiCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
